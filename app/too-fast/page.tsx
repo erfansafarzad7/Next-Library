c0a85e914e7254f9fa269527455b0a37eddf0f2b
Erfan Safarzad <erfansafarzad7@gmail.com>
2025-07-01 20:54:21 +0330

    feat: add ratelimit

---
+++ app/too-fast/page.tsx
@@ -0,0 +1,13 @@
+const Page = () => {
+  return (
+    <main className="root-container flex min-h-screen flex-col items-center justify-center">
+      <h1 className="font-bebas-neue text-5xl font-bold text-light-100">
+        There Is TooMany Requests!
+      </h1>
+      <p className="mt-3 max-w-xl text-center text-light-100">
+        Try again after a few minutes.
+      </p>
+    </main>
+  );
+};
+export default Page;
