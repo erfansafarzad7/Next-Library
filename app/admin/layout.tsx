ba59026fc9d8cadc43a63318a195093ac56cdd4b
Erfan Safarzad <erfansafarzad7@gmail.com>
2025-07-02 19:29:32 +0330

    feat: init admin page

---
+++ app/admin/layout.tsx
@@ -0,0 +1,24 @@
+import "@/styles/admin.css";
+import { ReactNode } from "react";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import Slidebar from "@/components/admin/Slidebar";
+import Header from "@/components/admin/Header";
+
+const Layout = async ({ children }: { children: ReactNode }) => {
+  const session = await auth();
+
+  if (!session?.user?.id) redirect("/sign-in");
+
+  return (
+    <main className="flex min-h-screen w-full flex-row">
+      <Slidebar session={session} />
+
+      <div className="admin-container">
+        <Header session={session} />
+        {children}
+      </div>
+    </main>
+  );
+};
+export default Layout;
