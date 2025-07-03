044ab20e4894ba57d2c1c494b837cb8997c04b47
Erfan Safarzad <erfansafarzad7@gmail.com>
2025-07-03 21:29:58 +0330

    refactor: edit header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,44 +1,45 @@
 "use client";
+
 import React from "react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { cn, getInitials } from "@/lib/utils";
 import Image from "next/image";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Session } from "next-auth";
+import { signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
 
 const Header = ({ session }: { session: Session }) => {
-  const pathname = usePathname();
-
   return (
     <header className="my-10 flex justify-between gap-5">
       <Link href="/">
         <Image src="/icons/logo.svg" alt="logo" width={40} height={40} />
       </Link>
 
-      <ul className="flex flex-row items-center gap-8">
-        <li>
-          <Link
-            href="/library"
-            className={cn(
-              "text-base cursor-pointer capitalize",
-              pathname === "/library" ? "text-light-200" : "text-light-100",
-            )}
-          >
-            Library
-          </Link>
-        </li>
+      {session && (
+        <ul className="flex flex-row items-center gap-8">
+          <li>
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+              className="mb-10"
+            >
+              <Button>Logout</Button>
+            </form>
+          </li>
 
-        <li>
-          <Link href="/my-profile">
-            <Avatar>
-              <AvatarFallback className="bg-amber-100">
-                {getInitials(session?.user?.name || "IN")}
-              </AvatarFallback>
-            </Avatar>
-          </Link>
-        </li>
-      </ul>
+          <li>
+            <Link href="/">
+              <Avatar>
+                <AvatarFallback className="bg-amber-100">
+                  {getInitials(session?.user?.name || "IN")}
+                </AvatarFallback>
+              </Avatar>
+            </Link>
+          </li>
+        </ul>
+      )}
     </header>
   );
 };
